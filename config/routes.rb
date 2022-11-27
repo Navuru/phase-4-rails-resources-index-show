2a373d1a2613ceb632375eb5536b628f3a954da7
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-11-27 12:32:23 +0300

    Add likes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   
   # get "/birds", to: "birds#index"
   # get "/birds/:id", to: "birds#show"
-  resources :birds ,only: [:index, :show, :create]
+  resources :birds ,only: [:index, :show, :create, :update]
+  patch "/birds/:id/like", to: "birds#increment_likes"
 end
