03396c5bc3bb05f4047ceb302742894f32653e50
Lisa Lyayuga <lisa.lyayuga@student.moringaschool.com>
2022-11-27 12:44:55 +0300

    Add delete functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   # get "/birds", to: "birds#index"
   # get "/birds/:id", to: "birds#show"
-  resources :birds ,only: [:index, :show, :create, :update]
+  # resources :birds ,only: [:index, :show, :create, :update, :destroy]
+  # since we are using all the routes the only can be emmitted
+  resources :birds
   patch "/birds/:id/like", to: "birds#increment_likes"
 end
